dc4d359e4b0a357657f092cc39520a98dbfdcf42
Muhammedabdelhey <muhammedabdelhay1@gmail.com>
2024-09-02 09:04:46 +0300

    update git ignote v2

--- E-Commerce.Application/obj/Debug/net8.0/E_Commerce.Application.AssemblyInfo.cs
+++ E-Commerce.Application/obj/Debug/net8.0/E_Commerce.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64bd8df669dcf8380482d9f74baaaa955046545")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff68af9b835827b08bded14d368aff826e5b8cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
