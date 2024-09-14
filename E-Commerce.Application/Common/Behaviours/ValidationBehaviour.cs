655db04824329e89488bcd0f57dd3741cc01d284
Muhammedabdelhey <muhammedabdelhay1@gmail.com>
2024-09-14 10:08:30 +0300

    add Validation Behavior

---
+++ E-Commerce.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,34 @@
+﻿namespace E_Commerce.Application.Common.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TRsponse> : IPipelineBehavior<TRequest, TRsponse>
+        where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TRsponse> Handle(TRequest request, RequestHandlerDelegate<TRsponse> next, CancellationToken cancellationToken)
+        {
+
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(
+                    _validators.Select(v =>
+                        v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults
+                    .Where(r => r.Errors.Any())
+                    .SelectMany(r => r.Errors)
+                    .ToList();
+
+                if (failures.Any())
+                    throw new ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
--- E-Commerce.Application/DependencyInjection.cs
+++ E-Commerce.Application/DependencyInjection.cs
@@ -1,11 +1,14 @@
-﻿namespace E_Commerce.Application
+﻿using E_Commerce.Application.Common.Behaviours;
+
+namespace E_Commerce.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddFluentValidationAutoValidation();
+            //services.AddFluentValidationAutoValidation();
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
