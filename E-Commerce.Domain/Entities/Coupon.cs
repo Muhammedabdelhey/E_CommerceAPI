cd2d5ecaa3935c5f229d7b0a3646e4f8fea596ef
Muhammedabdelhey <muhammedabdelhay1@gmail.com>
2024-10-03 13:06:32 +0300

    modify coupon DomainEntity

--- E-Commerce.Domain/Entities/Coupon.cs
+++ E-Commerce.Domain/Entities/Coupon.cs
@@ -6,6 +6,17 @@ namespace E_Commerce.Domain.Entities
     {
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
-
+        public int NumberOfUsing { get; set; } = 0;
+        public int MaxNumberOfUses { get; set; }
+        public decimal DiscountValue { get; set; }
+        public DiscountType DiscountType { get; set; }
+        public bool IsActive { get; set; } = true;
+        public int UsageLimitPerUser { get; set; }
+        public string CouponCode { get; set; } = string.Empty;
+    }
+    public enum DiscountType
+    {
+        Percentage,
+        FixedAmount
     }
 }
