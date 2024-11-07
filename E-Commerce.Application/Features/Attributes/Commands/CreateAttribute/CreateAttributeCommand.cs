587ab1077353a5f73dbcffd3290e973bd36cf3a8
Muhammedabdelhey <muhammedabdelhay1@gmail.com>
2024-11-07 09:37:34 +0200

    change name to Name

--- E-Commerce.Application/Features/Attributes/Commands/CreateAttribute/CreateAttributeCommand.cs
+++ E-Commerce.Application/Features/Attributes/Commands/CreateAttribute/CreateAttributeCommand.cs
@@ -1,4 +1,4 @@
 ﻿namespace E_Commerce.Application.Features.Attributes.Commands.CreateAttribute
 {
-    public record CreateAttributeCommand(string name) : IRequest<AttributeDto>;
+    public record CreateAttributeCommand(string Name) : IRequest<AttributeDto>;
 }
